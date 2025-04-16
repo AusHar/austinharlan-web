bd47f0da57d0b42c26656ac5c5eb9b9caa5f3c7d
Austin Harlan <aharl56@wgu.edu>
2025-04-15 23:20:26 -0500

    include base in vite config

--- austinharlan-web/vite.config.js
+++ austinharlan-web/vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path';
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // Ensures `@` maps to `src`
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
